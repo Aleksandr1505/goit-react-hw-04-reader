a14f734cd0f26f417d670270caa5103472a86d7c
Aleksandr <olexander.o.havrylenko@gmail.com>
2019-09-15 00:30:18 +0300

    added task

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Reader from './Reader/Reader';
+
+const App = () => {
+  return (
+    <>
+      <Switch>
+        <Route path="/reader" render={props => <Reader {...props} />} />
+        <Redirect
+          to={{
+            pathname: '/reader',
+            search: '?item=1',
+          }}
+        />
+      </Switch>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Reader/Reader.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import queryString from 'query-string';
+import Publication from '../Publication/Publication';
+import Counter from '../Counter/Counter';
+import Controls from '../Controls/Controls';
+import publicationsJson from '../publications.json';
+import styles from './Reader.module.css';
+
+const getItemFromLocation = location => queryString.parse(location.search).item;
+
+export default class Reader extends Component {
+  state = {
+    currentArticle: 1,
+    articles: publicationsJson,
+  };
+
+  componentDidMount() {
+    // const { currentArticle } = this.state;
+    const { location } = this.props;
+    const parsed = getItemFromLocation(location);
+
+    this.setState({ currentArticle: Number(parsed) });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { currentArticle } = this.state;
+    const { history, location } = this.props;
+    if (prevState.currentArticle !== currentArticle) {
+      history.push({
+        ...location,
+        search: `item=${currentArticle}`,
+      });
+    }
+  }
+
+  handleIncrement = () => {
+    this.setState(prevState => ({
+      currentArticle: prevState.currentArticle + 1,
+    }));
+  };
+
+  handleDecrement = () => {
+    this.setState(prevState => ({
+      currentArticle: prevState.currentArticle - 1,
+    }));
+  };
+
+  render() {
+    const { currentArticle, articles } = this.state;
+
+    return (
+      <div className={styles.reader}>
+        <Publication item={articles[currentArticle - 1]} />
+        <Counter
+          currentArticle={currentArticle}
+          articleQuantity={articles.length}
+        />
+        <Controls
+          onHandleIncrement={this.handleIncrement}
+          onHandleDecrement={this.handleDecrement}
+          disablePrev={currentArticle === 1 ? 'true' : 'false'}
+          disableNext={currentArticle === articles.length ? 'true' : 'false'}
+        />
+      </div>
+    );
+  }
+}
