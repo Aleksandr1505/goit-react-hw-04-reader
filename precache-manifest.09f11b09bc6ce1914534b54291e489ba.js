933fc51e0f153f0638a96a9393fc41ae13855e61
Aleksandr <olexander.o.havrylenko@gmail.com>
2019-09-15 00:36:32 +0300

    Updates

---
+++ precache-manifest.09f11b09bc6ce1914534b54291e489ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c94bd937a2793da54aa034a10573192",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "b432708300d83648d82e",
+    "url": "/goit-react-hw-04-reader/static/css/main.fc8f950a.chunk.css"
+  },
+  {
+    "revision": "8999c906bcef45459827",
+    "url": "/goit-react-hw-04-reader/static/js/2.49dc8b93.chunk.js"
+  },
+  {
+    "revision": "b432708300d83648d82e",
+    "url": "/goit-react-hw-04-reader/static/js/main.9de58787.chunk.js"
+  },
+  {
+    "revision": "fae253dca44c2159d04f",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.877735af.js"
+  }
+]);
